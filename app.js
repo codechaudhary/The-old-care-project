9ba5797010a72bc38fd14b36a2a3ea4d77f2acc6
Harshit chaudhary <101380165+codechaudhary@users.noreply.github.com>
2023-11-08 22:47:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
+
+// Code for connecting explore button with navbar
+
+// JavaScript for connecting explore button with navbar and smooth scrolling
+document.getElementById("explore-button").addEventListener("click", function (e) {
+  e.preventDefault();
+  const targetSection = document.getElementById("services");
+
+  // Get the navigation bar element
+  var navbar = document.getElementById("navbar");
+
+  // Toggle the "showNav" class to show/hide the navigation bar
+  navbar.classList.toggle("showNav");
+
+  // Scroll to the "services" section
+  targetSection.scrollIntoView({ behavior: "smooth" });
+});
